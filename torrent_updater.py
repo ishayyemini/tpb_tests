f6e4e31ed1ae4fc633ca112eb45e1cb06d7b2c99
Ishay <ishay.yemini@gmail.com>
2024-10-16 19:42:41 +0300

    Get shows from jellyfin's data

--- torrent_updater.py
+++ torrent_updater.py
@@ -1,79 +1,80 @@
-import json
-import re
 import os
+from datetime import date, timedelta
 
-from tmdbv3api import TMDb, Search, TV
+from jellyfin_api_client import AuthenticatedClient
+from jellyfin_api_client.api.items import get_items
+from jellyfin_api_client.api.tv_shows import get_episodes
+from jellyfin_api_client.models import ItemFields, BaseItemKind
 from tpblite import TPB
 from transmission_rpc import Client
 
 TV_SHOWS_FOLDER = "/mnt/media/Shows"
-CACHED_EPISODE = "cached.json"
-HOST = "192.168.1.204"
-USERNAME = os.environ["TRANSMISSION_USERNAME"]
-PASSWORD = os.environ["TRANSMISSION_PASSWORD"]
-TMDB_API_KEY = os.environ["TMDB_API_KEY"]
+TRANSMISSION_HOST = "192.168.1.204"
+TRANSMISSION_USERNAME = os.environ["TRANSMISSION_USERNAME"]
+TRANSMISSION_PASSWORD = os.environ["TRANSMISSION_PASSWORD"]
+JELLYFIN_SERVER = "http://192.168.1.206:8096"
+JELLYFIN_API_KEY = os.environ["JELLYFIN_API_KEY"]
 
-tmdb = TMDb()
-tmdb.api_key = TMDB_API_KEY
-search = Search(tmdb)
-tv = TV(tmdb)
 tpb = TPB()
-transmission = Client(host=HOST, port=9091, username=USERNAME, password=PASSWORD)
+transmission = Client(
+    host=TRANSMISSION_HOST,
+    port=9091,
+    username=TRANSMISSION_USERNAME,
+    password=TRANSMISSION_PASSWORD,
+)
+jellyfin = AuthenticatedClient(
+    base_url=JELLYFIN_SERVER,
+    token=f'Token="{JELLYFIN_API_KEY}"',
+    prefix="MediaBrowser",
+)
 
 
-def get_shows_from_folder():
-    return [f.name for f in os.scandir(TV_SHOWS_FOLDER) if f.is_dir()]
+def get_episodes_from_jellyfin():
+    missing_episodes = []
+    shows = get_items.sync(
+        client=jellyfin,
+        include_item_types=[BaseItemKind.SERIES],
+        fields=[ItemFields.PROVIDERIDS],
+        recursive=True,
+    ).items
+    for show in shows:
+        episodes = get_episodes.sync(client=jellyfin, series_id=show.id).items
+        for episode in episodes:
+            if (
+                episode.premiere_date
+                and episode.location_type == "Virtual"  # episode is missing
+                and (
+                    (date.today() - timedelta(days=7))
+                    <= episode.premiere_date.date()
+                    <= (date.today() - timedelta(days=1))
+                )  # aired sometime in the past week
+                and episode.parent_index_number > 0  # not a special
+            ):
+                missing_episodes.append(episode)
 
-
-def get_cached_episode(show):
-    if CACHED_EPISODE in [f.name for f in os.scandir(f"{TV_SHOWS_FOLDER}/{show}")]:
-        with open(f"{TV_SHOWS_FOLDER}/{show}/{CACHED_EPISODE}", "r") as openfile:
-            episode = json.load(openfile)
-    else:
-        episode = update_cached_episode(show)
-    return episode
-
-
-def update_cached_episode(show):
-    show_id = search.tv_shows(show)[0].id
-    next_episode = tv.details(show_id).next_episode_to_air
-    if next_episode is None:
-        return
-    episode = {
-        "season_number": next_episode["season_number"],
-        "episode_number": next_episode["episode_number"],
-        "air_date": next_episode["air_date"],
-        "show": show,
-    }
-    with open(f"{TV_SHOWS_FOLDER}/{show}/{CACHED_EPISODE}", "w") as openfile:
-        json.dump(episode, openfile)
-    return episode
+    return missing_episodes
 
 
 def find_torrent(episode):
-    search_term = f"{episode['show']} S{str(episode['season_number']).zfill(2)}E{str(episode['episode_number']).zfill(2)}"
-    regex = re.compile("^" + search_term.replace(" ", "[. ]"), re.IGNORECASE)
+    torrent = get_valid_torrent(episode, "hdr 2160p")
+    if torrent:
+        return torrent
+    torrent = get_valid_torrent(episode, "2160p")
+    if torrent:
+        return torrent
+    torrent = get_valid_torrent(episode, "1080p")
+    if torrent:
+        return torrent
 
+
+def get_valid_torrent(episode, suffix):
+    parsed_show_name = episode.series_name.replace("'", "")
+    search_term = f"{parsed_show_name} S{str(episode.parent_index_number).zfill(2)}E{str(episode.index_number).zfill(2)}"
     torrents = [
         torrent
-        for torrent in tpb.search(search_term + " hdr 2160p")
-        if regex.search(torrent.title)
-    ]
-    if len(torrents) > 0:
-        return torrents[0]
-
-    torrents = [
-        torrent
-        for torrent in tpb.search(search_term + " 2160p")
-        if regex.search(torrent.title)
-    ]
-    if len(torrents) > 0:
-        return torrents[0]
-
-    torrents = [
-        torrent
-        for torrent in tpb.search(search_term + " 1080p")
-        if regex.search(torrent.title)
+        for torrent in tpb.search(f"{search_term} {suffix}")
+        if ":" in torrent.upload_date.split(" ")[1]
+        and torrent.upload_date.split(" ")[0] >= episode.premiere_date.strftime("%m-%d")
     ]
     if len(torrents) > 0:
         return torrents[0]
@@ -84,17 +85,10 @@ def download_torrent(episode):
     if torrent:
         transmission.add_torrent(
             torrent.magnetlink,
-            download_dir=f"{TV_SHOWS_FOLDER}/{episode['show']}/Season {episode['season_number']}",
+            download_dir=f"{TV_SHOWS_FOLDER}/{episode.series_name}/Season {episode.parent_index_number}",
         )
-        return True
-    else:
-        return False
 
 
 if __name__ == "__main__":
-    show_names = get_shows_from_folder()
-
-    for show in show_names:
-        episode = get_cached_episode(show)
-        if episode is not None and download_torrent(episode):
-            update_cached_episode(show)
+    for ep in get_episodes_from_jellyfin():
+        download_torrent(ep)
